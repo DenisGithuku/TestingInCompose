8e6c6f08104622e36cbb5799eb64ad773ede97c0
Denis Githuku <githukudenis@gmail.com>
2023-03-07 15:21:44 +0300

    check start destination

---
+++ app/src/androidTest/java/com/githukudenis/testingincompose/NavigationTest.kt
@@ -0,0 +1,35 @@
+package com.githukudenis.testingincompose
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setUpNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            AppNavigator(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        composeTestRule
+            .onNodeWithText("Composer")
+            .assertIsDisplayed()
+    }
+}
