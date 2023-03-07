b5c75406b90ef4ea5ddca2d19646bf1ca0a90292
Denis Githuku <githukudenis@gmail.com>
2023-03-07 17:07:54 +0300

    test navigation actions

--- app/src/androidTest/java/com/githukudenis/testingincompose/NavigationTest.kt
+++ app/src/androidTest/java/com/githukudenis/testingincompose/NavigationTest.kt
@@ -3,10 +3,14 @@ package com.githukudenis.testingincompose
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -31,5 +35,17 @@ class NavigationTest {
         composeTestRule
             .onNodeWithText("Composer")
             .assertIsDisplayed()
+    }
+
+
+    @Test
+    fun appNavHost_clickSplashText_navigateToSplashScreen() {
+        composeTestRule
+            .onNodeWithText("Composer")
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(route, "splash")
     }
 }
